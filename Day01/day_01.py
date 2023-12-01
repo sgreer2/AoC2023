641ff21073635e061102fed5dacdb115228b3369
Steve <>
2023-12-01 12:56:22 -0500

    Completed both parts of Day 1.

--- Day01/day_01.py
+++ Day01/day_01.py
@@ -1,29 +1,76 @@
 
+from re import findall
+from math import inf
+
+
 def read_data():
     file = 'Day01/t_input.txt' if TESTING else 'Day01/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        return f.read().split('\n')[:-1]
 
 
-def p1(data):
-    pass
+def p1(lines: list[str]) -> int:
+    total = 0
+
+    for line in lines:
+        nums = findall(r'\d', line)
+        val = int(''.join([nums[0], nums[-1]]))
+        total += val
+
+    return total
 
 
-def p2(data):
-    pass
+def p2(lines: list[str]) -> int:
+    numbers = {
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine'
+    }
+    total = 0
+
+    for line in lines:
+        lowest = [inf, '']
+        highest = [-inf, '']
+        for k, v in numbers.items():
+            if k in line:
+                index = line.index(k)
+                rindex = line.rindex(k)
+                if index < lowest[0]:
+                    lowest = [index, k]
+                if rindex > highest[0]:
+                    highest = [rindex, k]
+
+            if v in line:
+                index = line.index(v)
+                rindex = line.rindex(v)
+                if index < lowest[0]:
+                    lowest = [index, k]
+                if rindex > highest[0]:
+                    highest = [rindex, k]
+
+        val = ''.join([lowest[1], highest[1]])
+        total += int(val)
+
+    return total
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    lines = read_data()
+    s1, s2 = p1(lines), p2(lines)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
 
-TESTING = True
+TESTING = False
 
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 55090
+# Part 2 solution: 54845
