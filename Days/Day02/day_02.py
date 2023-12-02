c5fdaf1b59982ea87c3385073c2b0552e6583d24
Steve <>
2023-12-02 00:53:04 -0500

    Completed both parts and tests for Day 2.

--- Days/Day02/day_02.py
+++ Days/Day02/day_02.py
@@ -1,21 +1,83 @@
 
+class Game:
+    id: int
+    rounds: list[list[tuple[int, str]]]
+    p1_valid: bool
+    p2_value: int
+
+    def __init__(self, line: str) -> None:
+
+        self._parse(line)
+        self._p1_validate()
+        self._p2_solve()
+
+    def _parse(self, data: str) -> None:
+        game_id, rounds = data.split(': ')
+
+        self.id = int(game_id.replace('Game ', ''))
+        self.rounds = []
+
+        for round in rounds.split('; '):
+            self.rounds.append([])
+            for pair in round.split(', '):
+                val, colour = pair.split(' ')
+                self.rounds[-1].append((int(val), colour))
+
+    def _p1_validate(self) -> None:
+        limits = {
+            'red': 12,
+            'green': 13,
+            'blue': 14
+        }
+        valid = True
+        for round in self.rounds:
+            for value, colour in round:
+                if value > limits[colour]:
+                    self.p1_valid = False
+                    return
+
+        self.p1_valid = valid
+
+    def _p2_solve(self) -> None:
+        colours = {}
+        for round in self.rounds:
+            for value, colour in round:
+                if colour not in colours.keys():
+                    colours[colour] = value
+                    continue
+                colours[colour] = max([value, colours[colour]])
+
+        total = 1
+        for c in colours.values():
+            total *= c
+        self.p2_value = total
+
+
 def read_data():
     file = 'Days/Day02/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        lines = f.read().split('\n')[:-1]
+    return [Game(line) for line in lines]
 
 
-def p1(data) -> int:
-    return -1
+def p1(games: list[Game]) -> int:
+    total = 0
+    for game in games:
+        if game.p1_valid:
+            total += game.id
+    return total
 
 
-def p2(data) -> int:
-    return -1
+def p2(games: list[Game]) -> int:
+    total = 0
+    for game in games:
+        total += game.p2_value
+    return total
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    games = read_data()
+    s1, s2 = p1(games), p2(games)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
@@ -23,5 +85,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 2683
+# Part 2 solution: 49710
--- Tests/day02_test.py
+++ Tests/day02_test.py
@@ -1,15 +1,25 @@
-from Days.Day02.day_02 import p1, p2
+from Days.Day02.day_02 import p1, p2, Game
 
 
 def test_p1():
     test_data = [
-
+        'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
     ]
-    assert p1(test_data) == -1
+    games = [Game(line) for line in test_data]
+    assert p1(games) == 8
 
 
 def test_p2():
     test_data = [
-
+        'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
     ]
-    assert p2(test_data) == -1
+    games = [Game(line) for line in test_data]
+    assert p2(games) == 2286
