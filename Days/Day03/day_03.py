9b2619c0d2d14d4e1d9358bc375802539241dbd4
Steve <>
2023-12-03 01:46:51 -0500

    Completed both parts and tests for Day 3.

--- Days/Day03/day_03.py
+++ Days/Day03/day_03.py
@@ -1,16 +1,71 @@
+from re import finditer
+
 
 def read_data():
     file = 'Days/Day03/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        return parse(f.read().split('\n')[:-1])
 
 
-def p1(data) -> int:
-    return -1
+def parse(array: list[str]) -> tuple[dict, dict]:
+    num_locs = dict()
+    sym_locs = dict()
+    for y, line in enumerate(array):
+
+        nums = finditer(r'\d+', line)
+        for num in nums:
+            num_locs[(y, num.span())] = int(num.group(0))
+
+        for x, c in enumerate(list(line)):
+            invalid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+            if c not in invalid:
+                sym_locs[(y, x)] = c
+
+    return (num_locs, sym_locs)
 
 
-def p2(data) -> int:
-    return -1
+def compare(num: tuple[int, tuple[int, int]], sym: tuple[int, int]) -> bool:
+    s_y, s_x = sym
+    n_y = num[0]
+    n_x = num[1]
+    if s_y > n_y+1 or s_y < n_y-1:  # If the Y pos is outside valid range
+        return False
+
+    if s_x < n_x[0]-1 or s_x > n_x[1]:  # If outside X range
+        return False
+
+    return True
+
+
+def p1(data: tuple[dict, dict]) -> int:
+    num_dict, sym_dict = data
+    total = 0
+    for n_loc, num in num_dict.items():
+        for s_loc, s in sym_dict.items():
+            if compare(n_loc, s_loc):
+                total += num
+                break
+
+    return total
+
+
+def p2(data: tuple[dict, dict]) -> int:
+    num_dict, sym_dict = data
+    total = 0
+    for s_loc, symbol in sym_dict.items():
+        if symbol != '*':
+            continue
+        nums = []
+        for n_loc, num in num_dict.items():
+            if compare(n_loc, s_loc):
+                nums.append(num)
+        if len(nums) != 2:
+            continue
+        temp = 1
+        for n in nums:
+            temp *= n
+        total += temp
+    return total
 
 
 def main():
@@ -23,5 +78,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 550934
+# Part 2 solution: 81997870
