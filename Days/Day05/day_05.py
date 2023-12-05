72e0436fae4004daed48e49f7adf4432432ffce0
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-05 13:33:13 -0500

    Completed both parts and tests for Day 5.

--- Days/Day05/day_05.py
+++ Days/Day05/day_05.py
@@ -1,21 +1,76 @@
+from re import findall
+
+
+class Map:
+    maps: list
+
+    def __init__(self, groups: list[list[str]]) -> None:
+        temp = []
+        for group in groups:
+            temp.append(list())
+            for line in group[1:]:
+                if line == '':
+                    break
+                nums = [int(n) for n in findall(r'\d+', line)]
+                t = [(nums[0], nums[0] + (nums[2])-1),
+                     (nums[1], nums[1] + (nums[2])-1)]
+                temp[-1].append(t)
+
+        self.maps = temp
+
+    def solve(self, val: int) -> int:
+        for map in self.maps:
+            for r in map:
+                v_min, v_max = r[1]
+                if val >= v_min and val <= v_max:
+                    index = val - v_min
+                    val = r[0][0] + index
+                    break
+        return val
+
+    def r_solve(self, number: int) -> int:
+        for map in self.maps[::-1]:
+            for r in map:
+                v_min, v_max = r[0]
+                if number >= v_min and number <= v_max:
+                    index = number - v_min
+                    number = r[1][0] + index
+                    break
+        return number
+
 
 def read_data():
     file = 'Days/Day05/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        groups = [g.split('\n') for g in f.read().split('\n\n')]
+    seeds = [int(n) for n in findall(r'\d+', groups[0][0])]
+    maps = Map(groups[1:])
+    return [seeds, maps]
 
 
-def p1(data) -> int:
-    return -1
+def p1(seeds: list[int], maps: Map) -> int:
+    locations = []
+    for seed in seeds:
+        locations.append(maps.solve(seed))
+    return min(locations)
 
 
-def p2(data) -> int:
-    return -1
+def p2(seeds: list[int], maps: Map) -> int:
+    seed_ranges = []
+    for i in range(0, len(seeds), 2):
+        seed_ranges.append((seeds[i], seeds[i] + (seeds[i+1])))
+    cur_number = 0
+    while True:
+        result = maps.r_solve(cur_number)
+        for seed_range in seed_ranges:
+            if seed_range[0] <= result <= seed_range[1]:
+                return cur_number
+        cur_number += 1
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    seeds, maps = read_data()
+    s1, s2 = p1(seeds, maps), p2(seeds, maps)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
@@ -23,5 +78,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 51580674
+# Part 2 solution: 99751240
--- Tests/day05_test.py
+++ Tests/day05_test.py
@@ -1,15 +1,93 @@
-from Days.Day05.day_05 import p1, p2
+from Days.Day05.day_05 import p1, p2, Map
 
 
 def test_p1():
-    test_data = [
-
+    seeds = [79, 14, 55, 13]
+    test_maps = [
+        [
+            'seed-to-soil map:',
+            '50 98 2',
+            '52 50 48'
+        ],
+        [
+            'soil-to-fertilizer map:',
+            '0 15 37',
+            '37 52 2',
+            '39 0 15'
+        ],
+        [
+            'fertilizer-to-water map:',
+            '49 53 8',
+            '0 11 42',
+            '42 0 7',
+            '57 7 4'
+        ],
+        [
+            'water-to-light map:',
+            '88 18 7',
+            '18 25 70'
+        ],
+        [
+            'light-to-temperature map:',
+            '45 77 23',
+            '81 45 19',
+            '68 64 13'
+        ],
+        [
+            'temperature-to-humidity map:',
+            '0 69 1',
+            '1 0 69'
+        ],
+        [
+            'humidity-to-location map:',
+            '60 56 37',
+            '56 93 4'
+        ]
     ]
-    assert p1(test_data) == -1
+    assert p1(seeds, Map(test_maps)) == 35
 
 
 def test_p2():
-    test_data = [
-
+    seeds = [79, 14, 55, 13]
+    test_maps = [
+        [
+            'seed-to-soil map:',
+            '50 98 2',
+            '52 50 48'
+        ],
+        [
+            'soil-to-fertilizer map:',
+            '0 15 37',
+            '37 52 2',
+            '39 0 15'
+        ],
+        [
+            'fertilizer-to-water map:',
+            '49 53 8',
+            '0 11 42',
+            '42 0 7',
+            '57 7 4'
+        ],
+        [
+            'water-to-light map:',
+            '88 18 7',
+            '18 25 70'
+        ],
+        [
+            'light-to-temperature map:',
+            '45 77 23',
+            '81 45 19',
+            '68 64 13'
+        ],
+        [
+            'temperature-to-humidity map:',
+            '0 69 1',
+            '1 0 69'
+        ],
+        [
+            'humidity-to-location map:',
+            '60 56 37',
+            '56 93 4'
+        ]
     ]
-    assert p2(test_data) == -1
+    assert p2(seeds, Map(test_maps)) == 46
