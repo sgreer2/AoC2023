09a995fe1725d280d7c0a8cbfb1ddce69b8edb1c
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-06 00:25:29 -0500

    Completed both parts and tests for Day 6.

--- Days/Day06/day_06.py
+++ Days/Day06/day_06.py
@@ -1,16 +1,47 @@
+from re import findall
+
 
 def read_data():
     file = 'Days/Day06/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        lines = f.read().split('\n')[:-1]
+    data = []
+    for line in lines:
+        data.append([int(num) for num in findall(r'\d+', line)])
+    return data
 
 
-def p1(data) -> int:
-    return -1
+def p1(data: list[list[int]]) -> int:
+    times, distances = data
+    wins = []
+
+    for i in range(len(times)):
+        time_limit = times[i]
+        distance_record = distances[i]
+        win_count = 0
+
+        for hold_time in range(1, time_limit-1):
+            distance_travel = hold_time * (time_limit-hold_time)
+            if distance_travel > distance_record:
+                win_count += 1
+        wins.append(win_count)
+
+    total = 1
+    for w in wins:
+        total *= w
+    return total
 
 
-def p2(data) -> int:
-    return -1
+def p2(data: list[list[int]]) -> int:
+    time_limit = int(''.join([str(num) for num in data[0]]))
+    distance_record = int(''.join([str(num) for num in data[1]]))
+    win_count = 0
+
+    for hold_time in range(1, time_limit-1):
+        distance_travel = hold_time * (time_limit-hold_time)
+        if distance_travel > distance_record:
+            win_count += 1
+    return win_count
 
 
 def main():
@@ -23,5 +54,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 771628
+# Part 2 solution: 27363861
--- Tests/day06_test.py
+++ Tests/day06_test.py
@@ -3,13 +3,15 @@ from Days.Day06.day_06 import p1, p2
 
 def test_p1():
     test_data = [
-
+        [7, 15, 30],
+        [9, 40, 200]
     ]
-    assert p1(test_data) == -1
+    assert p1(test_data) == 288
 
 
 def test_p2():
     test_data = [
-
+        [7, 15, 30],
+        [9, 40, 200]
     ]
-    assert p2(test_data) == -1
+    assert p2(test_data) == 71503
