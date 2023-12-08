4709b3747405e6aeeffc0f14d10629e32451a706
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-08 01:21:27 -0500

    Completed both parts and tests for Day 8.

--- Days/Day08/day_08.py
+++ Days/Day08/day_08.py
@@ -1,21 +1,50 @@
+from math import lcm
 
-def read_data():
+
+def read_data() -> tuple:
     file = 'Days/Day08/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        lines = f.read().split('\n')[:-1]
+    instructions = [0 if char == 'L' else 1 for char in lines[0]]
+    nodes = {}
+    for line in lines[2:]:
+        nodes[line[0:3]] = (line[7:10], line[12:15])
+    return (instructions, nodes)
 
 
-def p1(data) -> int:
-    return -1
+def _get_steps(instructions: list[int], nodes: dict, start: str = 'AAA', part2: bool = False) -> int:
+    cur_node = start
+    steps = 0
+    index = 0
+    while True:
+        if part2:
+            if cur_node[2] == 'Z':
+                return steps
+        else:
+            if cur_node == 'ZZZ':
+                return steps
+        cur_node = nodes[cur_node][instructions[index]]
+        index += 1
+        if index >= len(instructions):
+            index = 0
+        steps += 1
 
 
-def p2(data) -> int:
-    return -1
+def p1(instructions: list[int], nodes: dict) -> int:
+    return _get_steps(instructions, nodes)
+
+
+def p2(instructions: list[int], nodes: dict) -> int:
+    node_steps = []
+    for node in nodes.keys():
+        if node[2] == 'A':
+            node_steps.append(_get_steps(instructions, nodes, node, True))
+    return lcm(*node_steps)
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    instructions, nodes = read_data()
+    s1, s2 = p1(instructions, nodes), p2(instructions, nodes)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
@@ -23,5 +52,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 16343
+# Part 2 solution: 15299095336639
--- Tests/day08_test.py
+++ Tests/day08_test.py
@@ -2,14 +2,25 @@ from Days.Day08.day_08 import p1, p2
 
 
 def test_p1():
-    test_data = [
-
-    ]
-    assert p1(test_data) == -1
+    inst = [0, 0, 1]
+    nodes = {
+        'AAA': ('BBB', 'BBB'),
+        'BBB': ('AAA', 'ZZZ'),
+        'ZZZ': ('ZZZ', 'ZZZ')
+    }
+    assert p1(inst, nodes) == 6
 
 
 def test_p2():
-    test_data = [
-
-    ]
-    assert p2(test_data) == -1
+    inst = [0, 1]
+    nodes = {
+        '11A': ('11B', 'XXX'),
+        '11B': ('XXX', '11Z'),
+        '11Z': ('11B', 'XXX'),
+        '22A': ('22B', 'XXX'),
+        '22B': ('22C', '22C'),
+        '22C': ('22Z', '22Z'),
+        '22Z': ('22B', '22B'),
+        'XXX': ('XXX', 'XXX')
+    }
+    assert p2(inst, nodes) == 6
