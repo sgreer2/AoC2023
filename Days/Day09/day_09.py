085a6c1382dae50fa77f2f328b217e767c6a49f2
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-09 00:55:15 -0500

    Completed both parts and tests for Day 9.

--- Days/Day09/day_09.py
+++ Days/Day09/day_09.py
@@ -2,20 +2,63 @@
 def read_data():
     file = 'Days/Day09/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        lines = f.read().split('\n')[:-1]
+    sequences = []
+    for line in lines:
+        sequences.append([int(num) for num in line.split()])
+    return sequences
 
 
-def p1(data) -> int:
-    return -1
+def _allZeroes(array: list[int]) -> bool:
+    for num in array:
+        if num != 0:
+            return False
+    return True
 
 
-def p2(data) -> int:
-    return -1
+def p1(sequences: list[list[int]]) -> int:
+    next_value_sum = 0
+
+    for sequence in sequences:
+        history = [sequence]
+        while True:
+            if _allZeroes(history[-1]):
+                break
+            diffs = []
+            for i in range(len(history[-1])-1):
+                diffs.append(history[-1][i+1] - history[-1][i])
+            history.append(diffs)
+
+        next_value = 0
+        for line in history:
+            next_value += line[-1]
+        next_value_sum += next_value
+    return next_value_sum
+
+
+def p2(sequences: list[list[int]]) -> int:
+    next_value_sum = 0
+
+    for sequence in sequences:
+        history = [sequence]
+        while True:
+            if _allZeroes(history[-1]):
+                break
+            diffs = []
+            for i in range(len(history[-1])-1):
+                diffs.append(history[-1][i+1] - history[-1][i])
+            history.append(diffs)
+
+        next_value = 0
+        for h_index in range(len(history)-1, 0, -1):
+            next_value = history[h_index-1][0] - next_value
+        next_value_sum += next_value
+    return next_value_sum
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    sequences = read_data()
+    s1, s2 = p1(sequences), p2(sequences)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
@@ -23,5 +66,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
-# Part 2 solution:
+# Part 1 solution: 1884768153
+# Part 2 solution: 1031
--- Tests/day09_test.py
+++ Tests/day09_test.py
@@ -3,13 +3,17 @@ from Days.Day09.day_09 import p1, p2
 
 def test_p1():
     test_data = [
-
+        [0, 3, 6, 9, 12, 15],
+        [1, 3, 6, 10, 15, 21],
+        [10, 13, 16, 21, 30, 45]
     ]
-    assert p1(test_data) == -1
+    assert p1(test_data) == 114
 
 
 def test_p2():
     test_data = [
-
+        [0, 3, 6, 9, 12, 15],
+        [1, 3, 6, 10, 15, 21],
+        [10, 13, 16, 21, 30, 45]
     ]
-    assert p2(test_data) == -1
+    assert p2(test_data) == 2
