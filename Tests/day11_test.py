564f05fd4cce9cf687211831548b7b257bf1d353
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-10 22:50:23 -0500

    Ran create.py to make files for Day 11.

---
+++ Tests/day11_test.py
@@ -0,0 +1,15 @@
+from Days.Day11.day_11 import p1, p2
+
+
+def test_p1():
+    test_data = [
+
+    ]
+    assert p1(test_data) == -1
+
+
+def test_p2():
+    test_data = [
+
+    ]
+    assert p2(test_data) == -1
