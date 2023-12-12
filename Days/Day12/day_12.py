605455b2bd330a0a350520781b18e1d08fedef79
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-12 12:02:43 -0500

    Completed part 1 of Day 12.

--- Days/Day12/day_12.py
+++ Days/Day12/day_12.py
@@ -2,20 +2,67 @@
 def read_data():
     file = 'Days/Day12/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        return parse(f.read().split('\n')[:-1])
 
 
-def p1(data) -> int:
-    return -1
+def parse(array: list[str]) -> list[tuple[list[str], list[int]]]:
+    new_array = []
+    for line in array:
+        string, nums = line.split()
+        new_array.append((list(string), [int(num) for num in nums.split(',')]))
+
+    return new_array
 
 
-def p2(data) -> int:
+def _solve(spots: list[str], nums: list[int]) -> int:
+    total = 0
+
+    cur_num = nums[0]
+    next_nums = []
+
+    if len(nums) > 1:
+        next_nums = nums[1:]
+
+    for index, char in enumerate(spots):
+        if char == '.':
+            continue
+        if cur_num + index > len(spots):
+            break
+        place = spots[index:index+cur_num]
+        if '.' in place:
+            continue
+        if '#' in spots[:index]:
+            break
+        if index+cur_num != len(spots) and spots[index+cur_num] == '#':
+            continue
+
+        if len(next_nums) > 0:
+            total += _solve(spots[index+cur_num+1:], next_nums)
+        else:
+            if '#' in spots[index+cur_num+1:]:
+                continue
+            total += 1
+
+        if char == '#':
+            break
+
+    return total
+
+
+def p1(spring_data: list[tuple[list[str], list[int]]]) -> int:
+    total_arrangements = 0
+    for spots, nums in spring_data:
+        total_arrangements += _solve(spots, nums)
+    return total_arrangements
+
+
+def p2(spring_data: list[tuple[list[str], list[int]]]) -> int:
     return -1
 
 
 def main():
-    data = read_data()
-    s1, s2 = p1(data), p2(data)
+    spring_data = read_data()
+    s1, s2 = p1(spring_data), p2(spring_data)
     print(f'P1: {s1}')
     print(f'P2: {s2}')
 
@@ -23,5 +70,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Part 1 solution:
+# Part 1 solution: 7653
 # Part 2 solution:
--- Tests/day12_test.py
+++ Tests/day12_test.py
@@ -1,15 +1,20 @@
-from Days.Day12.day_12 import p1, p2
+from Days.Day12.day_12 import p1, p2, parse
 
 
 def test_p1():
     test_data = [
-
+        '???.### 1,1,3',
+        '.??..??...?##. 1,1,3',
+        '?#?#?#?#?#?#?#? 1,3,1,6',
+        '????.#...#... 4,1,1',
+        '????.######..#####. 1,6,5',
+        '?###???????? 3,2,1'
     ]
-    assert p1(test_data) == -1
+    assert p1(parse(test_data)) == 21
 
 
 def test_p2():
     test_data = [
 
     ]
-    assert p2(test_data) == -1
+    assert p2(parse(test_data)) == -1
