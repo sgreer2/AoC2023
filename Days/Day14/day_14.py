9e2e11a13b70c7b216f32cdc5e05b87cc79600fd
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-14 01:16:18 -0500

    Completed part 1 of Day 14.

--- Days/Day14/day_14.py
+++ Days/Day14/day_14.py
@@ -1,15 +1,58 @@
+from enum import IntEnum
+
 
 def read_data():
     file = 'Days/Day14/input.txt'
     with open(file, 'r') as f:
-        return f.read()
+        return f.read().split('\n')[:-1]
 
 
-def p1(data) -> int:
-    return -1
+class Direction(IntEnum):
+    North = 1
+    East = 2
+    South = 3
+    West = 4
 
 
-def p2(data) -> int:
+def _get_weight(array: list[str]) -> int:
+    total = 0
+    cur_val = len(array)
+    for line in array:
+        for char in line:
+            if char == 'O':
+                total += cur_val
+        cur_val -= 1
+    return total
+
+
+def _tilt(array: list[str], direction: Direction = Direction.North) -> list[str]:
+    new_array = []
+    for i in range(len(array)):
+        new_array.append(list(array[i]))
+    if direction == Direction.North:
+        for col_index in range(len(array[0])):
+            cur_col = [line[col_index] for line in array]
+            for char_index in range(1, len(cur_col)):
+                cur_index = char_index
+                while True:
+                    if cur_index <= 0:
+                        break
+                    if cur_col[cur_index] in ['#', '.']:
+                        break
+                    if cur_col[cur_index-1] == '.':
+                        cur_col[cur_index-1] = 'O'
+                        cur_col[cur_index] = '.'
+                    cur_index -= 1
+            for i, c in enumerate(cur_col):
+                new_array[i][col_index] = c
+    return new_array
+
+
+def p1(data: list[str]) -> int:
+    return _get_weight(_tilt(data))
+
+
+def p2(data: list[str]) -> int:
     return -1
 
 
--- Tests/day14_test.py
+++ Tests/day14_test.py
@@ -3,13 +3,22 @@ from Days.Day14.day_14 import p1, p2
 
 def test_p1():
     test_data = [
-
+        'O....#....',
+        'O.OO#....#',
+        '.....##...',
+        'OO.#O....O',
+        '.O.....O#.',
+        'O.#..O.#.#',
+        '..O..#O..O',
+        '.......O..',
+        '#....###..',
+        '#OO..#....'
     ]
-    assert p1(test_data) == -1
+    assert p1(test_data) == 136
 
 
-def test_p2():
-    test_data = [
+# def test_p2():
+#     test_data = [
 
-    ]
-    assert p2(test_data) == -1
+#     ]
+#     assert p2(test_data) == -1
