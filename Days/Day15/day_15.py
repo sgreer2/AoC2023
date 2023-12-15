0d3c020e50e377e1f6e142a3eeeb3813966f5a14
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-15 01:10:19 -0500

    Completed part 2 of Day 15.

--- Days/Day15/day_15.py
+++ Days/Day15/day_15.py
@@ -22,8 +22,45 @@ def p1(data: str) -> int:
     return total
 
 
-def p2(data) -> int:
-    return -1
+def p2(data: str) -> int:
+    boxes = dict()
+    for i in range(256):
+        boxes[i] = list()
+    for line in data.split(','):
+        if line[-1] == '-':
+            string = line[:-1]
+            box_id = string_to_hash(string)
+            for i in range(len(boxes[box_id])):
+                if boxes[box_id][i][0] == string:
+                    boxes[box_id].pop(i)
+                    break
+            continue
+        string, num = line.split('=')
+        num = int(num)
+        box_id = string_to_hash(string)
+        index_in_there = -1
+        for i in range(len(boxes[box_id])):
+            if boxes[box_id][i][0] == string:
+                index_in_there = i
+                break
+        if index_in_there == -1:
+            boxes[box_id].append((string, num))
+        else:
+            boxes[box_id][index_in_there] = (string, num)
+
+    total = 0
+    for box, lenses in boxes.items():
+        if lenses == []:
+            continue
+        box_values = []
+        for i in range(len(lenses)):
+            box_value = box + 1
+            focal_length = lenses[i][1]
+            box_value *= i+1
+            box_value *= focal_length
+            box_values.append(box_value)
+        total += sum(box_values)
+    return total
 
 
 def main():
@@ -37,4 +74,4 @@ if __name__ == '__main__':
     main()
 
 # Part 1 solution: 519603
-# Part 2 solution:
+# Part 2 solution: 244342
--- Tests/day15_test.py
+++ Tests/day15_test.py
@@ -11,8 +11,6 @@ def test_p1():
     assert p1(test_data) == 1320
 
 
-# def test_p2():
-#     test_data = [
-
-#     ]
-#     assert p2(test_data) == -1
+def test_p2():
+    test_data = 'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7'
+    assert p2(test_data) == 145
