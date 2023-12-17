c67ade348ff7242f6df3b8c9adb6afc7d131c64a
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-17 15:33:22 -0500

    Completed part 2 of Day 17.

--- Days/Day17/day_17.py
+++ Days/Day17/day_17.py
@@ -23,13 +23,15 @@ class Pos:
     y: int
     steps: int
     direction: D
+    history: list[D]
 
-    def __init__(self, value: int, pos: tuple[int, int], direction: D = D.Empty, steps: int = 0) -> None:
+    def __init__(self, value: int, pos: tuple[int, int], direction: D = D.Empty, steps: int = 0, history: list[D] = []) -> None:
         self.value = value
         self.x = pos[0]
         self.y = pos[1]
         self.direction = direction
         self.steps = steps
+        self.history = history
 
     def get_pos(self) -> tuple[int, int]:
         return (self.x, self.y)
@@ -43,17 +45,17 @@ class Pos:
                 if self.y == other.y:
                     if self.direction == other.direction:
                         return self.steps < other.steps
-                    return True
+                    return self.direction.value[0] < other.direction.value[0]
                 return self.y < other.y
             return self.x < other.x
         return self.value < other.value
 
 
-def p1(heat_map: list[list[int]]) -> int:
+def _solve(heat_map: list[list[int]], min_steps: int, max_steps: int) -> int:
     start = (0, 0)
-    _map_size = len(heat_map) - 1
-    end = (_map_size, _map_size)
-    max_steps = 3
+    _map_y_size = len(heat_map) - 1
+    _map_x_size = len(heat_map[0]) - 1
+    end = (_map_x_size, _map_y_size)
     visited: set[tuple[int, int, D, int]] = set()
     queue: PriorityQueue[Pos] = PriorityQueue()
     queue.put(Pos(0, start))
@@ -61,7 +63,7 @@ def p1(heat_map: list[list[int]]) -> int:
     while queue:
         current: Pos = queue.get()
 
-        if current.get_pos() == end:
+        if current.get_pos() == end and min_steps <= current.steps <= max_steps:
             return current.value
 
         if current.get_data() in visited:
@@ -73,16 +75,29 @@ def p1(heat_map: list[list[int]]) -> int:
             if direction == D.Empty:
                 continue
 
+            # Don't go backwards
+            if current.direction == D.North and direction == D.South:
+                continue
+            if current.direction == D.South and direction == D.North:
+                continue
+            if current.direction == D.East and direction == D.West:
+                continue
+            if current.direction == D.West and direction == D.East:
+                continue
+
             # Check bounds of new positions
             new_x = cur_x + direction.value[0]
             new_y = cur_y + direction.value[1]
-            if new_x < 0 or new_x > _map_size:
+            if new_x < 0 or new_x > _map_x_size:
                 continue
-            if new_y < 0 or new_y > _map_size:
+            if new_y < 0 or new_y > _map_y_size:
                 continue
 
             # Check number of steps
             new_steps = current.steps
+            if current.steps < min_steps:
+                if current.direction not in [D.Empty, direction]:
+                    continue
             if direction == current.direction:
                 new_steps += 1
             else:
@@ -90,13 +105,17 @@ def p1(heat_map: list[list[int]]) -> int:
             if new_steps > max_steps:
                 continue
             new_value = current.value + heat_map[new_y][new_x]
-            queue.put(Pos(new_value, (new_x, new_y), direction, new_steps))
-
+            queue.put(Pos(new_value, (new_x, new_y), direction,
+                      new_steps, current.history + [direction]))
     return -1
 
 
-def p2(data) -> int:
-    return -1
+def p1(heat_map: list[list[int]]) -> int:
+    return _solve(heat_map, 0, 3)
+
+
+def p2(heat_map: list[list[int]]) -> int:
+    return _solve(heat_map, 4, 10)
 
 
 def main():
@@ -110,4 +129,4 @@ if __name__ == '__main__':
     main()
 
 # Part 1 solution: 1195
-# Part 2 solution:
+# Part 2 solution: 1347
