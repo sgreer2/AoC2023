e520d07159386a04fd2b1911c7f74ae0c02f160f
Steve <7141756+sgreer2@users.noreply.github.com>
2023-12-24 22:04:50 -0500

    Ran create.py to make files for Day 25.

---
+++ Days/Day25/day_25.py
@@ -0,0 +1,27 @@
+
+def read_data():
+    file = 'Days/Day25/input.txt'
+    with open(file, 'r') as f:
+        return f.read()
+
+
+def p1(data) -> int:
+    return -1
+
+
+def p2(data) -> int:
+    return -1
+
+
+def main():
+    data = read_data()
+    s1, s2 = p1(data), p2(data)
+    print(f'P1: {s1}')
+    print(f'P2: {s2}')
+
+
+if __name__ == '__main__':
+    main()
+
+# Part 1 solution:
+# Part 2 solution:
